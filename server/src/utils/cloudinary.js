ed2700fc696d2b55d51bdf16f4f0fe821ef914f8
Saimr khan <itssameer75@gmail.com>
2024-07-29 14:27:01 +0530

    Configured Cloudinary for file uploads

---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"; //node file system
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return "File Not Found !" || null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    fs.unlinkSync(localFilePath); // remove the local save file as upload operation is successfull
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the local save file as upload operation failed
+
+    return null; // return null as upload operation failed
+  }
+};
+
+export { uploadOnCloudinary };
