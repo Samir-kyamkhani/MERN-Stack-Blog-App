4caae16fc1d52e94c2635d2ad79092278dc4fc92
Saimr khan <itssameer75@gmail.com>
2024-08-07 08:43:05 +0530

    removed file uploding from signup

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -17,25 +17,15 @@ import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.route("/signup").post(
-     //file handling middleware for multer, we can add multiple fields to the array. before userSignup Save
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-  ]),
-  userSignup,
-);
-
+router.route("/signup").post(userSignup,);
 router.route("/login").post(userLogin)
+router.route("/logout").post(verifyJwt, userLogout);
 
 // Secure logout route, we are using verifyJwt middleware to verify the jwt token.
-router.route("/logout").post(verifyJwt, userLogout);
-router.route("/refresh-token").post(refreshAccessToken);
+// router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJwt, changeCurrentPassword);
-router.route("/:_id").get(verifyJwt, getCurrentUser);
-router.route("/").get(verifyJwt, getAuthores);
+router.route("/:id").get(getCurrentUser);
+router.route("/").get(getAuthores);
 router.route("/update-account").patch(verifyJwt, updateUserDetails);
 
 router.route("/update-avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
